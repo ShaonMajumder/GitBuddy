2ba31ff8ada275a66b8b08cd6bc0a829c60d3b38
Shaon Majumder <smazoomder@gmail.com>
2020-03-30 22:26:12 +0600

    0.0.0.0.1;

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name = 'GitBuddy',
     packages = ['GitBuddy'],
-    version = '0.0.0.1.1',
+    version = 'GitBuddy',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author = 'Shaon Majumder',
     author_email = 'smazoomder@gmail.com',
     url = 'https://github.com/ShaonMajumder/GitBuddy',
-    download_url = 'https://github.com/ShaonMajumder/GitBuddy/archive/0.0.0.1.1.tar.gz',
+    download_url = 'https://github.com/ShaonMajumder/GitBuddy/archive/GitBuddy.tar.gz',
     keywords = ['Git Management', 'Shaon Majumder'],
     classifiers = [],
     setup_requires=['wheel']
